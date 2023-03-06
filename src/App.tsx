a3cb85fe4366c28ba417e930ce25bb15a3041df3
Zhi <neozhixuan@gmail.com>
2023-03-07 03:02:38 +0800

    Typescript/Tailwind Initialisation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
